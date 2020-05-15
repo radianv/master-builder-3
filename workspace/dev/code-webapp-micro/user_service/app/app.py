b3cd8a9bf9b455feaef26a11a3e0d3c1330fa7c1
Adrian <apaez@cloudera.com>
2020-05-15 01:49:43 -0500

    updating MySQL RDS cnn string

--- workspace/dev/code-webapp-micro/user_service/app/app.py
+++ workspace/dev/code-webapp-micro/user_service/app/app.py
@@ -14,7 +14,7 @@ login_manager.init_app(app)
 app.config.update(dict(
     SECRET_KEY="powerful secretkey",
     #SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:test@user_db/user',
-    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:*.101091Root*@user.ccrz0cn5l7sv.us-east-1.rds.amazonaws.com:3306/user',
+    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:*.101091Root*@mb3-db-user.ciiwt8d1nmks.ca-central-1.rds.amazonaws.com/user',
 ))
 
 models.init_app(app)
