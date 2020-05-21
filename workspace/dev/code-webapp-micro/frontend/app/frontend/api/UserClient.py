f2f23e138e74380feb9a544797668807ffb90d30
radianvstk@gmail.com <ec2-user@ip-172-31-8-106.ca-central-1.compute.internal>
2020-05-21 18:18:52 +0000

    Adding WebApp DNSLookUp going back V1

--- workspace/dev/code-webapp-micro/frontend/app/frontend/api/UserClient.py
+++ workspace/dev/code-webapp-micro/frontend/app/frontend/api/UserClient.py
@@ -1,9 +1,19 @@
 from flask import session
 import requests
+import dns.resolver
 
+def dns_resolve(domain):
+        srvInfo = {}
+        srv_records=dns.resolver.query('_user._tcp.'+domain, 'SRV')
+        for srv in srv_records:
+            srvInfo['weight']   = srv.weight
+            srvInfo['host']     = str(srv.target).rstrip('.')
+            srvInfo['priority'] = srv.priority
+            srvInfo['port']     = srv.port
+        return 'http://'+srvInfo['host']+':'+str(srvInfo['port'])
 
 class UserClient:
-
+    
     @staticmethod
     def post_login(form):
         api_key = False
@@ -11,7 +21,8 @@ class UserClient:
             'username': form.username.data,
             'password': form.password.data,
         }
-        url = 'http://user:5000/api/user/login'
+        url = dns_resolve('servicediscovery.internal')+'/api/user/login'
+        #url = 'http://ecs-s-ECSAL-1LSW6XFAGQQS3-818649976.ca-central-1.elb.amazonaws.com/api/user/login'
         response = requests.request("POST", url=url, data=payload)
         if response:
             d = response.json()
@@ -21,7 +32,8 @@ class UserClient:
 
     @staticmethod
     def does_exist(username):
-        url = 'http://user:5000/api/user/'+username+'/exist'
+        url = dns_resolve('servicediscovery.internal')+'/api/user/'+username+'/exist'
+        #url = 'http://ecs-s-ECSAL-1LSW6XFAGQQS3-818649976.ca-central-1.elb.amazonaws.com/api/user/'+username+'/exist'
         response = requests.request("GET", url=url)
         return response.status_code == 200
 
@@ -35,7 +47,8 @@ class UserClient:
             'last_name': form.last_name.data,
             'username': form.username.data
         }
-        url = 'http://user:5000/api/user/create'
+        url = dns_resolve('servicediscovery.internal')+'/api/user/create'
+        #url = 'http://ecs-s-ECSAL-1LSW6XFAGQQS3-818649976.ca-central-1.elb.amazonaws.com/api/user/create'
         response = requests.request("POST", url=url, data=payload)
         if response:
             user = response.json()
@@ -47,6 +60,7 @@ class UserClient:
             'Authorization': 'Basic ' + session['user_api_key']
         }
 
-        response = requests.request(method="GET", url='http://user:5000/api/user', headers=headers)
+        response = requests.request(method="GET", url=dns_resolve('servicediscovery.internal')+'/api/user', headers=headers)
+        #response = requests.request(method="GET", url='http://ecs-s-ECSAL-1LSW6XFAGQQS3-818649976.ca-central-1.elb.amazonaws.com/api/user', headers=headers)
         user = response.json()
         return user
