edcb7243dcc3bb465a89b289aca206455d962dc9
radianvstk@gmail.com <ec2-user@ip-172-31-8-106.ca-central-1.compute.internal>
2020-05-26 01:01:05 +0000

    Adding WebApp DNSLookUp Product

--- workspace/dev/code-webapp-micro/frontend/app/frontend/api/ProductClient.py
+++ workspace/dev/code-webapp-micro/frontend/app/frontend/api/ProductClient.py
@@ -1,16 +1,26 @@
 import requests
+import dns.resolver
 
+def dns_resolve(domain):
+        srvInfo = {}
+        srv_records=dns.resolver.query('_order._tcp.'+domain, 'SRV')
+        for srv in srv_records:
+            srvInfo['weight']   = srv.weight
+            srvInfo['host']     = str(srv.target).rstrip('.')
+            srvInfo['priority'] = srv.priority
+            srvInfo['port']     = srv.port
+        return 'http://'+srvInfo['host']+':'+str(srvInfo['port'])
 
 class ProductClient:
 
     @staticmethod
     def get_product(slug):
-        response = requests.request(method="GET", url='http://product:5000/api/product/' + slug)
+        response = requests.request(method="GET", url = dns_resolve('servicediscovery.internal')+'/api/product/' + slug)
         product = response.json()
         return product
 
     @staticmethod
     def get_products():
-        r = requests.get('http://product:5000/api/products')
+        r = requests.get(url = dns_resolve('servicediscovery.internal')+'/api/products')
         products = r.json()
         return products
